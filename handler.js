3875322412ea5c1321ea316dc857075aa0ab60b0
Sergiod3x <sergiod3x.@gmail.com>
2021-11-30 14:42:52 +0100

    fix

--- handler.js
+++ handler.js
@@ -3,17 +3,18 @@
 exports.graphql = (event, context, callback) => {
 	console.log('Received event {}', JSON.stringify(event, 3));
 
-	const consumerKey = event.arguments.consumer_key;
-  const selectKey = event.arguments.select_key;
-	const consumerSecret = event.arguments.consumer_secret;
+	const consumerKey = event.argumentos.consumer_key;
+  const selectKey = event.argumentos.select_key;
+	const consumerSecret = event.argumentos.consumer_secret;
+  const name = event.handle;
 
 	console.log('Got an Invoke Request.');
 	switch (selectKey) {
 		case 'helloWorld': {
-			callback(null, 'Hello world '+ ' Consumer Key: '+consumerKey+ " Consumer Secret: "+ consumerSecret);
+			callback(null, 'Hello world '+ ' Consumer Key: '+consumerKey+ " Consumer Secret: "+ consumerSecret + " name: "+ name);
     }
     case 'helloWorld2': {
-      callback(null, 'Hello world 2 '+ ' Consumer Key: '+consumerKey+ " Consumer Secret: "+ consumerSecret);
+      callback(null, 'Hello world 2 '+ ' Consumer Key: '+consumerKey+ " Consumer Secret: "+ consumerSecret+ " name " + name);
 			break;
 		}
 
