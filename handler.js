0f835b0431440298d27b678b60c904e757205a0e
Sergiod3x <sergiod3x.@gmail.com>
2021-11-30 11:24:39 +0100

    test 2.0

--- handler.js
+++ handler.js
@@ -4,10 +4,11 @@ exports.graphql = (event, context, callback) => {
 	console.log('Received event {}', JSON.stringify(event, 3));
 
 	const consumerKey = event.arguments.consumer_key;
+  const selectKey = event.arguments.select_key;
 	const consumerSecret = event.arguments.consumer_secret;
 
 	console.log('Got an Invoke Request.');
-	switch (event.field) {
+	switch (selectKey) {
 		case 'helloWorld': {
 			callback(null, 'Hello world '+ ' Consumer Key: '+consumerKey+ " Consumer Secret: "+ consumerSecret);
     }
