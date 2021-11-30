729931cde8f9c3f9c909c8d27609c9df8f6c9749
Sergiod3x <sergiod3x.@gmail.com>
2021-11-30 17:10:05 +0100

    update

--- handler.js
+++ handler.js
@@ -4,17 +4,17 @@ exports.graphql = (event, context, callback) => {
 	console.log('Received event {}', JSON.stringify(event, 3));
 
 	const consumerKey = event.argumentos.consumer_key;
-  const selectKey = event.argumentos.select_key;
 	const consumerSecret = event.argumentos.consumer_secret;
   const name = event.handle;
+  const selectK = event.selectK;
 
 	console.log('Got an Invoke Request.');
-	switch (selectKey) {
+	switch (selectK) {
 		case 'helloWorld': {
-			callback(null, 'Hello world '+ ' Consumer Key: '+consumerKey+ " Consumer Secret: "+ consumerSecret + " name: "+ name);
+			callback(null, 'Hello world '+ ' Consumer Key: '+consumerKey+ " Consumer Secret: "+ consumerSecret + " name: "+ name + "ecco la select K : "+selectK);
     }
     case 'helloWorld2': {
-      callback(null, 'Hello world 2 '+ ' Consumer Key: '+consumerKey+ " Consumer Secret: "+ consumerSecret+ " name " + name);
+      callback(null, 'Hello world 2 '+ ' Consumer Key: '+consumerKey+ " Consumer Secret: "+ consumerSecret+ " name " + name+ "ecco la select K : "+selectK);
 			break;
 		}
 
